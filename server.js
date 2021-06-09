7b54cb7544aa7b2de8fa06644a8374b9ee46753c
TJ Hutchinson <tjhutch92@gmail.com>
2021-06-08 19:35:33 -0700

    api and DB set up

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const server = require('http').Server(app);
-const next =  require('next');
-const dev = process.env.NODE_ENV!=='production';
-const nextApp = next({dev});
+const server = require("http").Server(app);
+const next = require("next");
+const dev = process.env.NODE_ENV !== "production";
+const nextApp = next({ dev });
 const handle = nextApp.getRequestHandler();
-require('dotenv').config({path:"./config.env"});
-const connectDb = require('./utilsServer/connectDb');
+require("dotenv").config({ path: "./config.env" });
+const connectDb = require("./utilsServer/connectDb");
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 connectDb();
 
 nextApp.prepare().then(() => {
-    app.all("*", (req, res) => handle(req, res));
+  app.use("/api/signup", require("./api/signup"));
+  app.use("/api/auth", require("./api/auth"));
 
-    server.listen(PORT,err => {
-        if (err) throw err;
-        console.log(`Express server running on ${PORT}`);
-    });
+  app.all("*", (req, res) => handle(req, res));
+
+  server.listen(PORT, (err) => {
+    if (err) throw err;
+    console.log(`Express server running on ${PORT}`);
+  });
 });
